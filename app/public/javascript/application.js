19406bc272485bd671c8d91d22fbf9101fb8c71d
Andrey Krahmal <coolrighthere@yahoo.com>
2014-02-19 12:05:16 +0000

    Catching the Button clicking on homepage

--- app/public/javascript/application.js
+++ app/public/javascript/application.js
@@ -4,11 +4,13 @@ function showSchitFavouritedNotice(schit) {
 	var favourited = !!$(schit).data("favourited");
 	var name = $(schit).find(".message").text();
 	var message = favourited ? name + " was added to favs" : name + " was removed from favs";   
-
+	var $flash = $("<div></div>").addClass('flash notice').html(message);
+	$flash.appendTo('#flash-container');
+	window.setTimeout(function() {
+		$flash.fadeOut();
+	}, 2000);
 	console.log(message);
-
 }
-
 
 function addFavouritesHandler() {
 	$('.flag.solid').click(function(event) {
@@ -21,9 +23,17 @@ function addFavouritesHandler() {
 	});
 }
 
+function prepareNewSchitHandler(){
+	$('.add-schit').click(function(event) {
+		$.get($(this).attr("href"), function(data) {
+			console.log(data);
+		});
+		event.preventDefault();
+	});
+}
 
 $(function() {
 	addFavouritesHandler();
-
+	prepareNewSchitHandler();
 })
 
